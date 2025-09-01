3f12ff922ae45985811b230b45b4561ef51643ba
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-01 22:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let allPrices = document.querySelectorAll(".price");
 
 const getSum = () => {
-//Add your code here
-  
+	let sum = 0;
+	for(let num of allPrices) {
+		sum += parseInt(num.textContent);
+	}
+	console.log(sum);
 };
 
 getSumBtn.addEventListener("click", getSum);
