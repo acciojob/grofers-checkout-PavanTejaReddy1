956a31fdbbe0e8458d7fffdf744544761ff3c55c
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-02 22:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+let table = document.getElementsByTagName("table")[0];
+let prices = document.querySelectorAll(".price");
+	
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-let allPrices = document.querySelectorAll(".price");
 
-const getSum = () => {
+getSumBtn.addEventListener("click", ()=>{
+	const tableRow = document.createElement("tr")
+	
+	const tableDataPrices = document.createElement("td");
+	tableDataPrices.textContent = "Total Price";
+	tableRow.appendChild(tableDataPrices);
+	
+	const tableDataSum = document.createElement("td");
+	tableDataSum.textContent = getSum();
+	tableRow.appendChild(tableDataSum);
+	
+	table.appendChild(tableRow);
+})
+
+function getSum() {
 	let sum = 0;
-	for(let num of allPrices) {
-		sum += parseInt(num.textContent);
+	for(let price of prices) {
+		sum += parseInt(price.textContent)
 	}
-	console.log(sum);
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+	return sum;
+}
