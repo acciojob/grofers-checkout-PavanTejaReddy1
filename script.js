b8f8124255ec21c0d710572f772b821de535b5dc
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-13 15:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ let table = document.getElementsByTagName("table")[0];
 let prices = document.querySelectorAll(".price");
 	
 const getSumBtn = document.createElement("button");
+getSumBtn.style.cursor = "pointer";
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 getSumBtn.addEventListener("click", ()=>{
+	const oldRow = document.getElementById("totalRow");
+	if(oldRow) {
+		oldRow.remove();
+	}
 	const tableRow = document.createElement("tr");
-	
+	tableRow.id = "totalRow"
 	const tableDataPrices = document.createElement("td");
-	tableDataPrices.textContent = `Total Price ${getSum()}`;
-	tableDataPrices.style.colspan = "2";
+	const tableDataPricesSum = document.createElement("td");
+	tableDataPrices.textContent = "Total Price";
+	tableDataPricesSum.textContent = getSum();
+	tableDataPricesSum.innerHTML = `<span id="ans">${getSum()}</span>`;
 	tableRow.appendChild(tableDataPrices);
-	
+	tableRow.appendChild(tableDataPricesSum);
 	table.appendChild(tableRow);
 })
 
